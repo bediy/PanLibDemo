25afc4166df39e6042d8514ad6246d9ada7a1fb5
Ye <ye7412357@gmail.com>
2017-11-16 01:58:09 +0800

    merge from two local branched test.(version 2)

--- panlibrary/src/main/java/com/example/panlibrary/PanView.java
+++ panlibrary/src/main/java/com/example/panlibrary/PanView.java
@@ -199,7 +199,7 @@ public class PanView extends View {
                 final double deltaDegrees = moveDegrees - downDegrees;
                 rotation = (int) (totalRotation + deltaDegrees);
                 Log.i(TAG, "/////" + totalRotation + "..." + deltaDegrees);
-                Log.i(TAG, "merge from two local branches." + totalRotation + "..." + deltaDegrees);
+                Log.i(TAG, "merge from two local branches.(version 2)" + totalRotation + "..." + deltaDegrees);
                 invalidate();
                 lastDegrees = deltaDegrees;
                 lastMoveDegrees = moveDegrees;
